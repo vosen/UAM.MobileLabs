33604f0d0545f5f89f95806207f9935b07a437ce
vosen <vosen@vosen.pl>
2012-06-13 15:56:34 +0200

    Changed computation model for TouchImageView and added border snapping.

--- Views/TouchImageView.cs
+++ Views/TouchImageView.cs
@@ -17,18 +17,95 @@ namespace ComicsViewer.Views
     public class TouchImageView : ImageView
     {
         static float[] ZoomSteps = new float[] { 0.25f, 0.33f, 0.5f, 0.67f, 0.75f, 0.9f, 1.0f, 1.25f, 1.5f, 1.75f, 2.5f, 2.5f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f };
-        const int ClickThreshold = 3;
         int CurrentStep;
-        float centerX, centerY;
+        Bitmap ImageSource { get; set; }
+        public float LeftMargin { get; set; }
+        public float RightMargin { get; set; }
+        public float TopMargin { get; set; }
+        public float BottomMargin { get; set; }
+        private float CurrentScale { get { return ZoomSteps[CurrentStep]; } }
+
+        float transX;
+        private float TranslationX
+        {
+            get
+            {
+                return transX;
+            }
+            set
+            {
+                transX = NormalizedTranslationX(value);
+            }
+        }
+
+        float transY;
+
+
+        private float TranslationY
+        {
+            get
+            {
+                return transY;
+            }
+            set
+            {
+                transY = NormalizedTranslationY(value);
+            }
+        }
+
+        private float NormalizedTranslationY(float value)
+        {
+            if (ImageSource == null)
+                return value;
+            float imageHeight = (ImageSource.Height * CurrentScale);
+            float topSpace = value - TopMargin;
+            float bottomSpace = Height - value - imageHeight;
+            if (topSpace > 0 && bottomSpace <= 0)
+            {
+                if (imageHeight >= Height)
+                    return TopMargin;
+                else
+                    return Height - BottomMargin - imageHeight;
+            }
+            if (topSpace <= 0 && bottomSpace > 0)
+            {
+                if (imageHeight >= Height)
+                    return Height - BottomMargin - imageHeight;
+                else
+                    return TopMargin;
+            }
+            return value;
+        }
+
+        private float NormalizedTranslationX(float value)
+        {
+            if (ImageSource == null)
+                return value;
+            float imageWidth = (ImageSource.Width * CurrentScale);
+            float leftSpace = value - LeftMargin;
+            float rightSpace = Width - value - imageWidth;
+            if (leftSpace > 0 && rightSpace <= 0)
+            {
+                if (imageWidth >= Width)
+                    // snap to left
+                    return LeftMargin;
+                else
+                    // snap to right
+                    return Width - RightMargin - imageWidth;
+            }
+            if (leftSpace <= 0 && rightSpace > 0)
+            {
+                if (imageWidth >= Width)
+                    // snap to right
+                    return Width - RightMargin - imageWidth;
+                else
+                    // snap to left
+                    return LeftMargin;
+            }
+            return value;
+        }
 
         float lastX, lastY;
-        float startX, startY;
-        float currentScale = 1f;
-        float right, bottom, origWidth, origHeight, bmWidth, bmHeight;
-        float[] rawMatrix = new float[9];
-        Bitmap ImageSource { get; set; }
-
-        float redundantXSpace, redundantYSpace;
 
         public TouchImageView(Context context, IAttributeSet attrs) :
             base(context, attrs)
@@ -46,14 +123,16 @@ namespace ComicsViewer.Views
         {
             Clickable = true;
             SetScaleType(ScaleType.Matrix);
-            CurrentStep = 3;
+            CurrentStep = 6;
+            TranslationX = 0;
+            TranslationY = 0;
             this.Touch += (src, args) => OnTouch(args);
         }
 
         protected override IParcelable OnSaveInstanceState()
         {
             IParcelable parcel = base.OnSaveInstanceState();
-            return new TouchImageViewState(parcel, centerX, centerY, CurrentStep);
+            return new TouchImageViewState(parcel, TranslationX, TranslationY, CurrentStep);
         }
 
         protected override void OnRestoreInstanceState(IParcelable state)
@@ -61,8 +140,8 @@ namespace ComicsViewer.Views
             TouchImageViewState savedState = state as TouchImageViewState;
             if (savedState != null)
             {
-                centerX = savedState.CenterX;
-                centerY = savedState.CenterY;
+                TranslationX = savedState.CenterX;
+                TranslationY = savedState.CenterY;
                 CurrentStep = savedState.ScaleStep;
             }
             base.OnRestoreInstanceState(savedState.SuperState);
@@ -70,34 +149,30 @@ namespace ComicsViewer.Views
 
         public void ZoomOut()
         {
+            float oldFactor = ZoomSteps[CurrentStep];
             CurrentStep = Math.Max(CurrentStep - 1, 0);
-            Zoom(ZoomSteps[CurrentStep]);
-            RefreshLayout(ZoomSteps[CurrentStep], centerX, centerY);
+            float newFactor = ZoomSteps[CurrentStep];
+            Zoom(oldFactor, newFactor);
         }
 
         public void ZoomIn()
         {
+            float oldFactor = ZoomSteps[CurrentStep];
             CurrentStep = Math.Min(CurrentStep + 1, ZoomSteps.Length - 1);
-            RefreshLayout(ZoomSteps[CurrentStep], centerX, centerY);
+            float newFactor = ZoomSteps[CurrentStep];
+            Zoom(oldFactor, newFactor);
         }
 
-        private void Zoom(float factor)
+        private void Zoom(float oldFactor, float newFactor)
         {
-            ImageMatrix.SetScale(ZoomSteps[CurrentStep], ZoomSteps[CurrentStep]);
-            ImageMatrix.PostTranslate(
-                centerX - ZoomSteps[CurrentStep] * (ImageSource.Width / 2f),
-                centerY - ZoomSteps[CurrentStep] * (ImageSource.Height / 2f));
-            //RefreshLayout();
-            Invalidate();
+            float oldDistLeft = (Width / 2) - TranslationX;
+            float oldDistTop = (Height / 2) - TranslationY;
+            float newDistLeft = (newFactor / oldFactor) * oldDistLeft;
+            float newDistTop = (newFactor / oldFactor) * oldDistTop;
+            TranslationX += (oldDistLeft - newDistLeft);
+            TranslationY += (oldDistTop - newDistTop);
+            RefreshLayout(ZoomSteps[CurrentStep], TranslationX, TranslationY);
         }
-
-        private void Move(float x, float y)
-        {
-            centerX += x;
-            centerY += y;
-            RefreshLayout(ZoomSteps[CurrentStep], centerX, centerY);
-        }
-
 
         void OnTouch(View.TouchEventArgs ev)
         {
@@ -108,11 +183,11 @@ namespace ComicsViewer.Views
                     lastY = ev.Event.GetY();
                     break;
                 case MotionEventActions.Move:
-                    centerX += ev.Event.GetX() - lastX;
-                    centerY += ev.Event.GetY() - lastY;
+                    TranslationX += ev.Event.GetX() - lastX;
+                    TranslationY += ev.Event.GetY() - lastY;
                     lastX = ev.Event.GetX();
                     lastY = ev.Event.GetY();
-                    RefreshLayout(ZoomSteps[CurrentStep], centerX, centerY);
+                    RefreshLayout(ZoomSteps[CurrentStep], TranslationX, TranslationY);
                     break;
             }
             ev.Handled = true;
@@ -135,20 +210,13 @@ namespace ComicsViewer.Views
             base.OnMeasure(widthMeasureSpec, heightMeasureSpec);
             if (ImageSource == null)
                 return;
-
-            centerX = Width / 2f;
-            centerY = Height / 2f;
-            // just center the image
-            //ImageMatrix.SetRectToRect(new RectF(0, 0, ImageSource.Width, ImageSource.Height), new RectF(0, 0, Width, Height), Matrix.ScaleToFit.Center);
-            RefreshLayout(ZoomSteps[CurrentStep], centerX, centerY);
+            RefreshLayout(ZoomSteps[CurrentStep], TranslationX, TranslationY);
         }
 
         private void RefreshLayout(float scale, float centerX, float centerY)
         {
             ImageMatrix.SetScale(scale, scale);
-            ImageMatrix.PostTranslate(
-                centerX - scale * (ImageSource.Width / 2f),
-                centerY - scale * (ImageSource.Height / 2f));
+            ImageMatrix.PostTranslate(centerX, centerY);
             ImageMatrix = ImageMatrix;
             Invalidate();
         }
