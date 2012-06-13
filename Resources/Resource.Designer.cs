d812aaaf04f66e739973378cc9a6a8115edda979
vosen <vosen@vosen.pl>
2012-06-13 13:25:51 +0200

    Changed layout slightly.

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -52,11 +52,14 @@ namespace ComicsViewer
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050004
+			public const int ButtonsLayout = 2131034116;
+			
 			// aapt resource value: 0x7f050001
 			public const int CurrentPath = 2131034113;
 			
-			// aapt resource value: 0x7f050004
-			public const int LeftButton = 2131034116;
+			// aapt resource value: 0x7f050006
+			public const int LeftButton = 2131034118;
 			
 			// aapt resource value: 0x7f050003
 			public const int MainImage = 2131034115;
@@ -64,17 +67,17 @@ namespace ComicsViewer
 			// aapt resource value: 0x7f050002
 			public const int MyButton = 2131034114;
 			
-			// aapt resource value: 0x7f050005
-			public const int OpenButton = 2131034117;
+			// aapt resource value: 0x7f050007
+			public const int OpenButton = 2131034119;
 			
-			// aapt resource value: 0x7f050006
-			public const int RightButton = 2131034118;
+			// aapt resource value: 0x7f050008
+			public const int RightButton = 2131034120;
 			
 			// aapt resource value: 0x7f050000
 			public const int RowText = 2131034112;
 			
-			// aapt resource value: 0x7f050007
-			public const int ZoomButtons = 2131034119;
+			// aapt resource value: 0x7f050005
+			public const int ZoomButtons = 2131034117;
 			
 			private Id()
 			{
